44ee4771e842ef67cfd2ee2f69a9039c592b3b08
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2025-05-04 22:37:06 +0300

    fix cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
-import {generatorHandler} from './index.js';
+import {generatorHandler} from '@prisma/generator-helper';
+import {onGenerate} from './on-generate.js';
+import {onManifest} from './on-manifest.js';
 
-// Execute the generator handler when called directly
-// This ensures the CLI works when installed
-generatorHandler();
+generatorHandler({
+	onManifest,
+	onGenerate,
+});
