2136bda0aadbf80429500d2b6006872d22523663
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2025-08-26 11:30:40 +0300

    fix: add support to array scalars

--- src/on-generate/generate-properties-from-model.ts
+++ src/on-generate/generate-properties-from-model.ts
@@ -73,7 +73,15 @@ export function generatePropertiesFromModel(
 					}
 				}
 
-				property = scalarProperty;
+				if (field.isList) {
+					property = {
+						type: 'array',
+						items: scalarProperty,
+					};
+				} else {
+					property = scalarProperty;
+				}
+
 				break;
 			}
 
